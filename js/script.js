3418987c9995a4e785a3418616bc329494311f14
bethanyhung <bethany_hung@brown.edu>
2017-08-06 17:45:35 -0700

    enhanced dragging experience

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
 	$('#name').hover(function(){
 		$(this).toggleClass('highlight');
-	});	
+	});
+
+	$(function() {
+		$('#name').hover(function() { 
+    		$('#appear').fadeIn(); 
+		}, function() { 
+    		$('#appear').fadeOut(); 
+		});
+	});
+
 	$('#name').draggable();
-	// $('button').click(function(){
 		var bounce = $('#bounce');
 		runIt();
    		function runIt(){
        		bounce.animate({top:'+=10'}, 750);
        		bounce.animate({top:'-=10'}, 750, runIt);
    		}	
-	// });
 });
