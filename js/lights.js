f1fcada36cc72b66e62e487f190ffcaca8e46617
bethanyhung <bethany_hung@brown.edu>
2018-01-03 16:33:32 -0800

    neonnn

---
+++ js/lights.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var $timing = (1000);
+
+	$('.hover-space').hover(
+		function() {
+			$('#lit').fadeIn(0);
+			$('body').animate({backgroundColor:"#000000"},$timing);
+			// $('#unlit').fadeOut($timing);
+			$('#unlit').css("display","none");
+		}, function() {
+			// $('#lit').fadeOut($timing);
+			$('#lit').css("display","none");
+			$('body').animate({backgroundColor:"#fff6d9"},$timing);
+			$('#unlit').fadeIn(0);
+	}); 
+});
