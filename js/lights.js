f1dda880ef8781c22e1973866ff298d19585251e
bethanyhung <bethany_hung@brown.edu>
2018-01-20 21:53:32 -0800

    art upd8

--- js/lights.js
+++ js/lights.js
@@ -4,10 +4,13 @@ $(document).ready(function(){
 	$('.hover-space').hover(
 		function() {
 			$('#lit').fadeIn(0);
+			$('.show-when-dark').fadeIn(1000).hide(0).delay(200).show(0).delay(100).hide(0).delay(100).show(0);
 			$('body').animate({backgroundColor:"#000000"},$timing);
 			$('#unlit').css("display","none");
+
 		}, function() {
 			$('#lit').css("display","none");
+			$('.show-when-dark').css("display","none");;
 			$('body').animate({backgroundColor:"#fff6d9"},$timing);
 			$('#unlit').fadeIn(0);
 	}); 
